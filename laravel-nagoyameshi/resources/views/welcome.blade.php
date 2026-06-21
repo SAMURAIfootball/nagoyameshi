b50ef0f66e66d4120f41f16f6ca325bee193d292
SAMURAIfootball <weitenghonger@gmail.com>
2026-06-22 08:16:52 +0900

    Update welcome.blade.php

--- laravel-nagoyameshi/resources/views/welcome.blade.php
+++ laravel-nagoyameshi/resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <!-- 修正後：この通りに書き換えて保存してください -->
+　　　　　<title>Laravel【検証環境テスト成功】</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
