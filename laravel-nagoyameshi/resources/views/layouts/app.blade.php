a09bad99a42bfae527c9186378782482d78a6702
SAMURAIfootball <weitenghonger@gmail.com>
2026-06-24 11:29:48 +0900

    Update app.blade.php

--- laravel-nagoyameshi/resources/views/layouts/app.blade.php
+++ laravel-nagoyameshi/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>NAGOYAMESHI検証環境</title>
+    <title>NAGOYAMESHI</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
